d1b278524e7d5c20e117f1efcf35dc99224c8832
Cih2008 <140653776+Cih2008@users.noreply.github.com>
2024-07-24 08:35:12 -0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,6 @@
+function comprarAgora() {
+   const widht = 800;
+   const height = 600;
+   const left = (screen.width/2)-(width/2);
+   const top = (screen.height/2)-(height/2);
+ window.open('pagamento.html','pagamento', 'width=${width}, height=${height}, top=${top},left=${left}');}
